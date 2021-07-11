076e9d934d68d982e60ac420b4117742cabc4247
Sebastian Correa <correa.sebas6@gmail.com>
2021-07-10 20:23:19 -0500

    Adding the onDecimalPoint function

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -8,6 +8,10 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric = false
+    var lastDot = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,12 +19,21 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View){
         tvInput.append((view as Button).text)
-
+        lastNumeric = true
     }
 
     fun onClear(view: View) {
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
 
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 
 }
